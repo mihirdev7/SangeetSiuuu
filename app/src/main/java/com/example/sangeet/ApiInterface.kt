41e7d0fdc8ac6697a489e45511a7945fd1a9f840
mihirdev7 <manthandev07@gmail.com>
2024-02-02 16:49:52 +0530

    Removed API key

--- app/src/main/java/com/example/sangeet/ApiInterface.kt
+++ app/src/main/java/com/example/sangeet/ApiInterface.kt
@@ -6,8 +6,7 @@ import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface ApiInterface {
-    @Headers("X-RapidAPI-Key:677f430d47msh02029b10f4abba1p112695jsnce7724db7fa4",
-            "X-RapidAPI-Host:deezerdevs-deezer.p.rapidapi.com")
+    @Headers("//your api key")
     @GET("search")
     fun getData(@Query("q") query: String): Call<MyData>
 }
